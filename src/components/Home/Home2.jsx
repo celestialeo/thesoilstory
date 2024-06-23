8e4afb49a56f3e1252fd3d11a5434857c1b94861
Darshhhhh <contact.shahdarsh@gmail.com>
2024-06-23 14:41:57 -0400

    Home Page Done

---
+++ src/components/Home/Home2.jsx
@@ -0,0 +1,32 @@
+import home2 from "../../assets/HomePicturescollage.png";
+function Home2() {
+  return (
+    <div className="py-10 px-20 flex justify-center">
+      <div className="w-[50%]">
+        <h2
+          style={{ fontFamily: "Caveat" }}
+          className="text-[#4BAF47] text-4xl"
+        >
+          About Us
+        </h2>
+        <h1 className="font-semibold text-5xl mt-2">Get to know us</h1>
+        <p className="w-[70%] mt-7">
+          Welcome to The Soil Story, where your weekends get a rustic twist!
+          We're all about swapping city chaos for farm-fresh fun. From milking
+          cows to picking strawberries, we connect you with real farm
+          adventures. Join us to dig into nature, embrace the countryside, and
+          create unforgettable memories. Let’s make farm life your new favourite
+          story! 🌾🐄🌻
+        </p>
+        <button className="px-4 py-2 text-sm bg-[#4BAF47] text-white rounded-md mt-6">
+          Discover More
+        </button>
+      </div>
+      <div className="w-[40%] text-start">
+        <img src={home2} alt="home2Image" />
+      </div>
+    </div>
+  );
+}
+
+export default Home2;
--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,9 +1,15 @@
 import Navbar from "../Navbar/Navbar";
 import Home1 from "./Home1";
+import Home2 from "./Home2";
+import Home3 from "./Home3";
+import Home4 from "./Home4";
 function HomePage() {
   return (
     <div>
       <Home1 />
+      <Home2 />
+      <Home3 />
+      <Home4 />
       <Navbar />
     </div>
   );
