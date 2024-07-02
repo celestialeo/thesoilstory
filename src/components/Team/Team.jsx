6265b157582c29514974fd44eba72d64e15e5f2a
celestialeo <Pavani.Miglani@georgebrown.ca>
2024-07-02 00:16:18 -0400

    team edits

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 import { GoArrowLeft } from "react-icons/go";
 import { GoArrowRight } from "react-icons/go";
 import Navbar from "../Navbar/Navbar";
+
 function Team() {
   const TeamData = [
     {
@@ -52,9 +53,9 @@ function Team() {
     <>
       <div className="w-full h-full relative">
         <Header text="white" />
-        <div className="flex justify-between px-40  items-center gap-10">
+        <div className="flex justify-between px-40 items-center gap-10">
           <button
-            className=" hover:bg-gray-400 text-gray-800 font-bold rounded-full p-1 border "
+            className="hover:bg-gray-400 text-gray-800 font-bold rounded-full p-1 border"
             onClick={handlePrevious}
           >
             <GoArrowLeft />
@@ -73,10 +74,13 @@ function Team() {
             <p className="w-[80%] mt-6">{TeamData[current]?.about}</p>
           </div>
           <div>
-            <img src={TeamData[current]?.image} />
+            <img
+              src={TeamData[current]?.image}
+              className="w-40 h-40 rounded-full object-cover"
+            />
           </div>
           <button
-            className=" hover:bg-gray-400 text-gray-800 font-bold rounded-full p-1 border"
+            className="hover:bg-gray-400 text-gray-800 font-bold rounded-full p-1 border"
             onClick={handleNext}
           >
             <GoArrowRight />
@@ -85,17 +89,15 @@ function Team() {
         <div className="flex justify-start z-10 py-5 glassEffect2 absolute bottom-0 w-full rounded-none rounded-t-lg px-36 gap-14">
           {TeamData?.map((data, idx) => {
             return (
-              <>
-                <div className="text-center hover:scale-[1.1]" key={idx}>
-                  <img
-                    src={data?.image}
-                    alt={data?.shortName}
-                    className="w-20 h-20 rounded-full object-cover mx-auto"
-                  />
-                  <div className="font-bold mt-4">{data?.shortName}</div>
-                  <div className="text-gray-600 text-sm">{data?.shortTag}</div>
-                </div>
-              </>
+              <div className="text-center hover:scale-[1.1]" key={idx}>
+                <img
+                  src={data?.image}
+                  alt={data?.shortName}
+                  className="w-20 h-20 rounded-full object-cover mx-auto"
+                />
+                <div className="font-bold mt-4">{data?.shortName}</div>
+                <div className="text-gray-600 text-sm">{data?.shortTag}</div>
+              </div>
             );
           })}
         </div>
