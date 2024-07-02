873e53bed201d9b37ec2e38116d195300c613a46
celestialeo <Pavani.Miglani@georgebrown.ca>
2024-07-02 16:05:19 -0400

    font edit

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -16,7 +16,7 @@ function Team() {
       shortName: "Sankar V Murali",
       tag: "Founder & Chief Executive Officer (CEO)",
       shortTag: "Founder & CEO",
-      about: "Sankar is the visionary behind The Soil Story. With a deep-rooted passion for sustainable agriculture and community empowerment, he founded The Soil Story to connect urban populations with the rural heart of India. Sankar holds an MSc in Innovation, Entrepreneurship, and Management from Imperial College Business School. His extensive experience in agricultural projects, including the successful Project Zaraat, has directly influenced over 3000 farmers by implementing sustainable and economically viable farming solutions. Sankar's leadership and dedication to sustainable development are the driving forces behind The Soil Story's mission.",
+      about: "Sankar is the visionary behind The Soil Story.With a deep-rooted passion for sustainable agriculture and community empowerment,he founded The Soil Story to connect urban populations with the rural heart of India.Sankar holds an MSc in Innovation, Entrepreneurship, and Management from Imperial College Business School. His extensive experience in agricultural projects, including the successful Project Zaraat, has directly influenced over 3000 farmers by implementing sustainable and economically viable farming solutions. Sankar's leadership and dedication to sustainable development are the driving forces behind The Soil Story's mission.",
       image: img1,
       avatar: img6,
     },
@@ -36,7 +36,7 @@ function Team() {
       tag: "Frontend  Developer",
       shortTag: "Frontend  Developer",
       about:
-        "Pavani Miglani is the technical backbone of The Soil Story. Her journey began as the Head of Research and Development at Project Zaraat, where she led groundbreaking initiatives to mitigate post-harvest losses and empower small and marginal farmers through innovative solutions. This role not only deepened her connection to agriculture but also honed her analytical and problem-solving skills. Currently, Pavani excels as a Full-Stack Developer in her full-time role, mastering both frontend and backend technologies. At The Soil Story, she channels her extensive technical expertise into creating seamless, responsive, and user-friendly web interfaces. Her dedication to technical excellence and user-centric design drives our mission to revolutionize agriculture through cutting-edge technology.",
+        "Pavani Miglani is the technical backbone of The Soil Story. Her journey began as the Head of Research and Development at Project Zaraat, where she led groundbreaking initiatives to mitigate post-harvest losses and empower small and marginal farmers through innovative solutions.Currently, Pavani excels as a Full-Stack Developer in her full-time role, mastering both frontend and backend technologies. At The Soil Story, she channels her extensive technical expertise into creating seamless, responsive, and user-friendly web interfaces. Her dedication to technical excellence and user-centric design drives our mission to revolutionize agriculture through cutting-edge technology.",
       image: img3,
       avatar: img4,
     },
