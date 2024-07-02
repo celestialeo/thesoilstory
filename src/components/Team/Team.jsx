67991afbf931f23c5eb56a0c24a6ff6ac7335d03
celestialeo <Pavani.Miglani@georgebrown.ca>
2024-07-02 16:28:27 -0400

    font edit

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -9,6 +9,7 @@ import { useState } from "react";
 import { GoArrowLeft } from "react-icons/go";
 import { GoArrowRight } from "react-icons/go";
 import Navbar from "../Navbar/Navbar";
+
 function Team() {
   const TeamData = [
     {
@@ -16,7 +17,8 @@ function Team() {
       shortName: "Sankar V Murali",
       tag: "Founder & Chief Executive Officer (CEO)",
       shortTag: "Founder & CEO",
-      about: "Sankar is the visionary behind The Soil Story.With a deep-rooted passion for sustainable agriculture and community empowerment,he founded The Soil Story to connect urban populations with the rural heart of India.Sankar holds an MSc in Innovation, Entrepreneurship, and Management from Imperial College Business School. His extensive experience in agricultural projects, including the successful Project Zaraat, has directly influenced over 3000 farmers by implementing sustainable and economically viable farming solutions. Sankar's leadership and dedication to sustainable development are the driving forces behind The Soil Story's mission.",
+      about:
+        "Sankar is the visionary behind The Soil Story. With a deep-rooted passion for sustainable agriculture and community empowerment, he founded The Soil Story to connect urban populations with the rural heart of India. Sankar holds an MSc in Innovation, Entrepreneurship, and Management from Imperial College Business School. His extensive experience in agricultural projects, including the successful Project Zaraat, has directly influenced over 3000 farmers by implementing sustainable and economically viable farming solutions. Sankar's leadership and dedication to sustainable development are the driving forces behind The Soil Story's mission.",
       image: img1,
       avatar: img6,
     },
@@ -36,7 +38,7 @@ function Team() {
       tag: "Frontend  Developer",
       shortTag: "Frontend  Developer",
       about:
-        "Pavani Miglani is the technical backbone of The Soil Story. Her journey began as the Head of Research and Development at Project Zaraat, where she led groundbreaking initiatives to mitigate post-harvest losses and empower small and marginal farmers through innovative solutions.Currently, Pavani excels as a Full-Stack Developer in her full-time role, mastering both frontend and backend technologies. At The Soil Story, she channels her extensive technical expertise into creating seamless, responsive, and user-friendly web interfaces. Her dedication to technical excellence and user-centric design drives our mission to revolutionize agriculture through cutting-edge technology.",
+        "Pavani Miglani is the technical backbone of The Soil Story. Her journey began as the Head of Research and Development at Project Zaraat, where she led groundbreaking initiatives to mitigate post-harvest losses and empower small and marginal farmers through innovative solutions. Currently, Pavani excels as a Full-Stack Developer in her full-time role, mastering both frontend and backend technologies. At The Soil Story, she channels her extensive technical expertise into creating seamless, responsive, and user-friendly web interfaces. Her dedication to technical excellence and user-centric design drives our mission to revolutionize agriculture through cutting-edge technology.",
       image: img3,
       avatar: img4,
     },
@@ -56,52 +58,45 @@ function Team() {
     <>
       <div className="w-full h-full relative">
         <Header text="white" />
-        <div className="flex justify-between px-40  items-center gap-10">
+        <div className="flex flex-col md:flex-row justify-between px-4 md:px-40 items-center gap-10">
           <button
-            className=" hover:bg-gray-400 text-gray-800 font-bold rounded-full p-1 border "
+            className="hover:bg-gray-400 text-gray-800 font-bold rounded-full p-1 border"
             onClick={handlePrevious}
           >
             <GoArrowLeft />
           </button>
-          <div className="w-full mt-[-10%]">
-            <h3
-              style={{ fontFamily: "Caveat" }}
-              className="text-[#4BAF47] text-4xl mb-5"
-            >
+          <div className="w-full mt-4 md:mt-0 text-center md:text-left">
+            <h3 style={{ fontFamily: "Caveat" }} className="text-[#4BAF47] text-4xl mb-5">
               Our Team
             </h3>
-            <h1 className="text-6xl font-semibold mb-2">
+            <h1 className="text-3xl md:text-6xl font-semibold mb-2">
               {TeamData[current]?.name}
             </h1>
             <h6 className="text-sm text-[#714015]">{TeamData[current]?.tag}</h6>
-            <p className="w-[80%] mt-6">{TeamData[current]?.about}</p>
+            <p className="w-full md:w-[80%] mt-6">{TeamData[current]?.about}</p>
           </div>
-          <div>
-            <img src={TeamData[current]?.image} />
+          <div className="w-40 md:w-auto">
+            <img src={TeamData[current]?.image} className="w-full h-auto" alt={TeamData[current]?.name} />
           </div>
           <button
-            className=" hover:bg-gray-400 text-gray-800 font-bold rounded-full p-1 border"
+            className="hover:bg-gray-400 text-gray-800 font-bold rounded-full p-1 border"
             onClick={handleNext}
           >
             <GoArrowRight />
           </button>
         </div>
-        <div className="flex justify-start z-10 py-5 glassEffect2 absolute bottom-0 w-full rounded-none rounded-t-lg px-36 gap-14">
-          {TeamData?.map((data, idx) => {
-            return (
-              <>
-                <div className="text-center hover:scale-[1.1] " key={idx}>
-                  <img
-                    src={data?.avatar}
-                    alt={data?.shortName}
-                    className="mx-auto rounded-full w-[77px]"
-                  />
-                  <div className="font-bold mt-4">{data?.shortName}</div>
-                  <div className="text-gray-600 text-sm">{data?.shortTag}</div>
-                </div>
-              </>
-            );
-          })}
+        <div className="flex justify-start z-10 py-5 glassEffect2 absolute bottom-0 w-full rounded-none rounded-t-lg px-4 md:px-36 gap-4 md:gap-14">
+          {TeamData.map((data, idx) => (
+            <div className="text-center hover:scale-[1.1]" key={idx}>
+              <img
+                src={data.avatar}
+                alt={data.shortName}
+                className="mx-auto rounded-full w-16 md:w-[77px]"
+              />
+              <div className="font-bold mt-4">{data.shortName}</div>
+              <div className="text-gray-600 text-sm">{data.shortTag}</div>
+            </div>
+          ))}
         </div>
       </div>
       <Navbar />
@@ -110,4 +105,3 @@ function Team() {
 }
 
 export default Team;
- 
