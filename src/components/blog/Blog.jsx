39ce9fdef85eb2d0bac310595d43349f260bf8c0
Darshhhhh <contact.shahdarsh@gmail.com>
2024-07-03 15:04:13 -0400

    blog bug fix

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -2,13 +2,19 @@ import Header from "../Header/Header";
 import Logo from "../../assets/logo.png";
 import JoinWaitlist from "../Utils/JoinWaitlist";
 import Navbar from "../Navbar/Navbar";
+import { useNavigate } from "react-router-dom";
 
 function Blog() {
+  const navigate = useNavigate();
+  const navigateTo = (link) => {
+    window.scrollTo(0, 0);
+    navigate(link);
+  };
   return (
     <>
       <Header text={"white"} />
       <div className="flex-col flex items-center my-20">
-        <img src={Logo} />
+        <img src={Logo} onClick={() => navigateTo("/")} className="hover:cursor-pointer"/>
         <h1
           style={{ fontFamily: "Caveat" }}
           className="text-[#4BAF47] text-7xl mb-7"
